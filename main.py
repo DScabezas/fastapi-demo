ab7cf11524f7eb31c1f88b5bbc87ced0d042991f
Dario <dariosce99@gmail.com>
2025-11-03 19:47:37 -0500

    Users endpoint get

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+users_db = ["Alice", "Bob", "Charlie"]
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/users")
+def get_users():
+    return {"users": users_db}
